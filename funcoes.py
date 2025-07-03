a2ae91de75907470e98acc2dbbf026cf50cdafe2
ghostoficial601 <pablo.pmlf@gmail.com>
2025-07-03 12:46:53 -0300

    adicionei a funcoes de codigos de calculadora e adicionei codigos a interface

--- funcoes.py
+++ funcoes.py
@@ -2,3 +2,12 @@
 
 def soma(x,y):
     return x + y
+
+def subtracao(x,y):
+    return x - y
+
+def divisao(x,y):
+    return x / y
+
+def multiplicacao(x,y):
+    return x*y
--- interface.py
+++ interface.py
@@ -1,2 +1,18 @@
 # aqui vai ser a interface:
+from funcoes import soma, subtracao, divisao, multiplicacao
+while True:
+    quest = str(input("O que vc gostaria de usar:\n0.sair \n1.+\n2.-\n3.%\n4.x\n--> "))
+    x_ = int(input("escreva um numero: "))
+    y_ = int(input("escreva um numero: "))
+    if quest in ["0", "sair"]:
+        break
+    elif quest in ["1", "+"]:
+        print(soma(x=x_, y=y_))
+    elif quest in ["2", "-"]:
+        print(subtracao(x=x_, y=y_))
+    elif quest in ["3", "%"]:
+        print(divisao(x=x_, y=y_))
+    elif quest in ["4", "x", "*"]:
+        print(multiplicacao(x=x_, y=y_))
 
+    
